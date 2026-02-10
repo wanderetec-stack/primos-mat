6f461fb619d3f34a0248d6c2dc13244148405ba0
Primos Mat Dev <dev@primos.mat.br>
2026-02-10 14:48:55 -0300

    fix: remove unused import causing build failure and preventing deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve, dirname } from 'path'
 import { fileURLToPath } from 'url'
-import obfuscatorPlugin from 'rollup-plugin-obfuscator';
+// import obfuscatorPlugin from 'rollup-plugin-obfuscator';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
